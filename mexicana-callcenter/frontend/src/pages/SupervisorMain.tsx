50fd89ba8f786a8b70a7ed9de8584740b769739d
fernicortlo <a01026613@tec.mx>
2024-05-14 17:48:45 -0600

    fixes typo

--- mexicana-callcenter/frontend/src/pages/SupervisorMain.tsx
+++ mexicana-callcenter/frontend/src/pages/SupervisorMain.tsx
@@ -55,7 +55,7 @@ const MainContent = () => {
     <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
       <HomeButton icon="/MetricsSymbol.svg" title="My Metrics" subtitle="See the real time metrics for all the agents" handleClick={() => window.location.href = '/Metrics'}/>
       <HomeButton icon="/SpotlightSymbol.svg" title="Agent Spotlight" subtitle="Weekly best agents" handleClick={() => window.location.href = '/AgentSpotlight'}/>
-      <HomeButton icon="/costumer.svg" title="Custumer Service" subtitle="See the metrics for the costumer service surveys" handleClick={() => window.location.href = '/CustomerService'}/>
+      <HomeButton icon="/costumer.svg" title="Customer Service" subtitle="See the metrics for the costumer service surveys" handleClick={() => window.location.href = '/CustomerService'}/>
       <HomeButton icon="/BreakSymbol.svg" title="Take a break" subtitle="Go to take a break to clear the mind" handleClick={() => window.location.href = '/Breaks'}/>
     </div>
     <SupervisorButton handleClick={() => window.location.href = '/supervisor/onGoingCalls'} />
